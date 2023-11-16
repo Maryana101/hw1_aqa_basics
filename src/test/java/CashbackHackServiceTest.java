5d78a01a0fba4a1e45c361e85dc2d9b252d64cf5
Maryana Varfolomeeva <mihralievam@icloud.com>
2023-11-17 00:37:51 +0500

    update tests with JUnit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,34 +1,33 @@
+import org.junit.Assert;
 import ru.netology.service.CashbackHackService;
 
 
-import static org.testng.Assert.*;
-
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn100ThenAmountIs900() {
         CashbackHackService cashBack = new CashbackHackService();
         int amount = 900;
         int actual = cashBack.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn0ThenAmountIs1000() {
         CashbackHackService cashBack = new CashbackHackService();
         int amount = 1000;
         int actual = cashBack.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn900ThenAmountIs1100() {
         CashbackHackService cashBack = new CashbackHackService();
         int amount = 1100;
         int actual = cashBack.remain(amount);
         int expected = 900;
-        assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
 }
